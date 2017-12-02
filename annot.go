c5900ba11bf2c3916d5619fca0a4c6f4c8ac75fa
matrix <zw_luowei@126.com>
2017-12-02 14:06:04 +0800

    add command-line

--- annot.go
+++ annot.go
@@ -2,13 +2,14 @@ package main
 
 import (
     "fmt"
+    "os"
 
     "github.com/union-cni/pkg/client"
 )
 
 func main() {
-    cli := client.CreateInsecureClient("127.0.0.1", "80")
-    anns, err := cli.GetAnnotations("default", "v9-simware")
+    cli := client.CreateInsecureClient("127.0.0.1", "8080")
+    anns, err := cli.GetAnnotations(os.Args[1], os.Args[2])
 
     fmt.Printf("Annotaions: %v, err %v\r\n", anns, err)
 }
