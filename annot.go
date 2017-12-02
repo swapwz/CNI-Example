f858ec0ece40ff6c2a81d62f3468079af79fc739
matrix <zw_luowei@126.com>
2017-12-02 10:58:04 +0800

    fixed client bug

--- annot.go
+++ annot.go
@@ -10,5 +10,5 @@ func main() {
     cli := client.CreateInsecureClient("127.0.0.1", "80")
     anns, err := cli.GetAnnotations("default", "v9-simware")
 
-    fm.Printf("Annotaions: %v, err %v", anns, err)
+    fmt.Printf("Annotaions: %v, err %v\r\n", anns, err)
 }
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,26 +4,27 @@ import (
     "fmt"
 
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
+ 
+    "k8s.io/api/core/v1"
     "k8s.io/client-go/kubernetes"
     "k8s.io/client-go/rest"
 )
 
 type Client struct {
-    Host string,
-    Port string,
+    Host string
+    Port string
     Config *rest.Config
 }
 
 func CreateInsecureClient(host, port string) *Client {
     client := &Client{
         Host: host,
-        Port: port
+        Port: port,
     }
 
-    config := &rest.Config(
-        Host: fmt.Sprintf("http://%s:%s", host, port)
-    ) 
+    config := &rest.Config{
+        Host: fmt.Sprintf("http://%s:%s", host, port),
+    } 
    
     config = rest.AnonymousClientConfig(config)
     client.Config = config
@@ -31,18 +32,18 @@ func CreateInsecureClient(host, port string) *Client {
     return client
 }
 
-func (cli *Client) GetAnnotations(namespace, podname string) (map[string][]string, error) {
+func (cli *Client) GetAnnotations(namespace, podname string) (*v1.Pod, error) {
     clientset, err := kubernetes.NewForConfig(cli.Config)
     if err != nil {
         return nil, fmt.Errorf("Create client failed: %v", err)
     }
 
-    pod, err := clientset.CoreV1().Pods(namespace).Get(podname, metav1.GetOptions())
+    pod, err := clientset.CoreV1().Pods(namespace).Get(podname, metav1.GetOptions{})
     if err != nil {
         return nil, fmt.Errorf("No such pod %s in namespace %s", podname, namespace)
     }
 
-    return pod.Annotations, nil
+    return pod, nil
 }
 
  
